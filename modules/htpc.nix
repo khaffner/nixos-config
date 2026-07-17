0c4714898ac880e9349e7d4bfbca4689a1bf7251
Kevin Haffner <khaffner@gmail.com>
2026-07-17 20:50:38 +0200

    make it possible to log in..

--- modules/htpc.nix
+++ modules/htpc.nix
@@ -14,11 +14,16 @@
   services.displayManager.autoLogin.enable = false;
 
   # Dedicated account for couch/TV use.
+  # Use a temporary initial password so GDM can list and authenticate
+  # the account cleanly. Change it after first login.
   users.users.htpc = {
     isNormalUser = true;
+    createHome = true;
     description = "HTPC";
+    home = "/home/htpc";
     extraGroups = [ "audio" "video" ];
     shell = pkgs.bash;
+    initialPassword = "htpc";
   };
 
   # Media center components for Kodi + CEC remote support
