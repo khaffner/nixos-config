e223a2a9a90dfda4f99a1ec6308b708ea047bae1
Kevin Haffner <khaffner@gmail.com>
2026-09-07 23:14:25 +0200

    path fix

--- configuration-STEAM.nix
+++ configuration-STEAM.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hosts/STEAM/hardware-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
