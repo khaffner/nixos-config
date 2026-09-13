11b5d615da5b14c70bbfe3e9c355c75592f5efb3
Kevin Haffner <khaffner@gmail.com>
2026-09-13 22:59:41 +0200

    remove duplicates

--- configuration-STEAM.nix
+++ configuration-STEAM.nix
@@ -20,11 +20,6 @@
 
   networking.hostName = "STEAM"; # Define your hostname.
 
-  # Configure console keymap
-  console.keyMap = "no";
-
-  nixpkgs.config.allowUnfree = true;  # Allow proprietary software such as Steam
-
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.05"; # Did you read the comment? DO NOT EDIT
 
